028b1e3a65e945fd9bb4ed9111cee31f064c72ed
wang wei <wangwei@comp.nus.edu.sg>
2014-12-15 16:21:22 +0800

    draft to calc precision and recall as yunchao's paper, but the recall and precsion would be the same...wait for his answer

--- src/caffe/layers/precision_recall_layer.cpp
+++ src/caffe/layers/precision_recall_layer.cpp
@@ -14,6 +14,7 @@ template <typename Dtype>
 void PrecisionRecallLayer<Dtype>::LayerSetUp(
   const vector<Blob<Dtype>*>& bottom, vector<Blob<Dtype>*>* top) {
   top_k_ = this->layer_param_.pr_param().top_k();
+   std::uniform_int_distribution<int> distribution_(0,80);
 }
 
 template <typename Dtype>
@@ -52,9 +53,17 @@ void PrecisionRecallLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom
     // sort label id in ascending order
     // std::sort(bottom_data_vector.begin(), bottom_data_vector.begin()+top_k_);
     Dtype ncorrect = 0, ntotal=0;
+    vector<int> groundtruth;
     // check if true label is in top k predictions
-    for(int j=0;bottom_label[label_dim*i+j]!=-1&&j<label_dim;j++){
+    for(int j=0;bottom_label[label_dim*i+j]!=-1&&j<std::min(top_k_, label_dim);j++){
       int label=static_cast<int>(bottom_label[label_dim*i+j]);
+      groundtruth.push_back(label);
+    }
+    while(groundtruth.size()<top_k_){
+      groundtruth.push_back(caffe_rng_rand()%81);
+    }
+    for(int j=0;j<top_k_;j++){
+      int label=groundtruth[j];
       for (int k = 0; k < top_k_; k++) {
         if (bottom_data_vector[k].second == label){
           ++ncorrect;
@@ -65,7 +74,7 @@ void PrecisionRecallLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom
     }
     CHECK_GT(ntotal,0);
     precision+=ncorrect/top_k_;
-    recall+=ncorrect/ntotal;
+    recall+=ncorrect/top_k_;
   }
 
   // precision
